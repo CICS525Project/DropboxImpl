bdceb4a962259c29bc851be9425bbe6c1540054c
ignaciopq <ignaciopq@gmail.com>
2014-07-14 21:58:50 +0000

    ServerApp
    uploading server app files

---
+++ ServerApp/ServerApp/src/authentication/Test.java
@@ -0,0 +1,11 @@
+package authentication;
+
+public class Test {
+	
+	public static void main(String args[]) {
+		Authentication auth = new Authentication();
+		
+		System.out.println(auth.createUser("john", "321"));
+	}
+
+}
