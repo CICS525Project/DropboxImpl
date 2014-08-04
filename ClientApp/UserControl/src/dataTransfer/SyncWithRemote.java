016018e87b2166bd826e5b8652a3e5f89408a7b5
Haonan Xu <xhn0701@gmail.com>
2014-08-04 12:14:01 -0700

    polling bug fixed

--- ClientApp/UserControl/src/dataTransfer/SyncWithRemote.java
+++ ClientApp/UserControl/src/dataTransfer/SyncWithRemote.java
@@ -26,10 +26,40 @@ public class SyncWithRemote implements Runnable {
 		start();
 	}
 
-	private void pollInfo(){
+	private void pollSharedFileInit(){
+		System.out.println("Function polling shared called");
+		try {
+			Registry registry = LocateRegistry.getRegistry(SessionInfo
+					.getInstance().getRemoteDNS(), SessionInfo.getInstance()
+					.getPortNum());
+			this.serviceProvider = (ServiceServerInterface) registry
+					.lookup("cloudboxRMI");
+			HashMap<String, Integer> remoteFileAccess = serviceProvider.getCurrentFiles(SessionInfo.getInstance().getUsername());
+			ArrayList<String> remoteFile = (ArrayList<String>) remoteFileAccess.keySet();
+			ArrayList<String> localFile = fopt.getFileInFolder(SessionInfo.getInstance().getWorkFolder());
+			for(String file : remoteFile){
+				//if local file does not have file in remote
+				//means this file is just shared from others
+				System.out.println("File: " + file + " is shared to me.");
+				if(!localFile.contains(file)){
+					OperationQueue.getInstance().add(file, OperationQueue.getInstance().getDownloadQueue());
+				}
+			}
+			
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void pollSharedFileModify(){
 		//get shared file from remote
 		//get corresponding files in local
 		//check if the version number is different
+		System.out.println("Function polling modify called");
 		try {
 			Registry registry = LocateRegistry.getRegistry(SessionInfo
 					.getInstance().getRemoteDNS(), SessionInfo.getInstance()
@@ -43,11 +73,13 @@ public class SyncWithRemote implements Runnable {
 			for(String fn : filesLocal){
 				//if the local file is a shared file
 				if(filesAndVersionShared.containsKey(fn)){
+					System.out.println("Detect in polling, shared file " + fn);
 					String localVersion = cmd.readVersionForOne(fn);
 					int localVersionInt = Integer.parseInt(localVersion);
 					int remoteVersion = filesAndVersionShared.get(fn);
 					String filePath = SessionInfo.getInstance().getWorkFolder() + File.separator + fn;
 					if(remoteVersion == -1){
+						System.out.println("Detect file deleted.");
 						fopt.deleteFileInFoler(filePath);
 						ArrayList<String> f = new ArrayList<String>();
 						//according remote, deleting local and remove local meta data
@@ -55,6 +87,7 @@ public class SyncWithRemote implements Runnable {
 						cmd.removeRecord(filePath, f);
 					}
 					if(remoteVersion > localVersionInt){
+						System.out.println("Detect file changed.");
 						//download if remote version is greater than the local version
 						OperationQueue.getInstance().add(fn, OperationQueue.getInstance().getDownloadQueue());
 					}
@@ -77,7 +110,8 @@ public class SyncWithRemote implements Runnable {
 		// TODO Auto-generated method stub
 		while(true){
 			try {
-				pollInfo();
+				pollSharedFileInit();
+				pollSharedFileModify();
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
